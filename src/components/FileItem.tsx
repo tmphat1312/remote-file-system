53115d122347be40ad2160bb656e3923823c7492
Truong Minh Phat <tmphat1312@gmail.com>
2025-02-11 13:34:51 +0000

    Upgrade TailwindCSS to version 4

--- src/components/FileItem.tsx
+++ src/components/FileItem.tsx
@@ -21,7 +21,7 @@ export function FileItem({ file, onFolderClick }: FileItemProps) {
       className={`flex items-center p-2 hover:bg-gray-700 rounded cursor-pointer ${file.type === "folder" ? "text-blue-400" : "text-gray-300"}`}
     >
       <Icon className="mr-2" size={18} />
-      <span className="flex-grow">{file.name}</span>
+      <span className="grow">{file.name}</span>
       {file.type === "file" && (
         <>
           <span className="text-sm text-gray-500 mr-4">{file.size}</span>
